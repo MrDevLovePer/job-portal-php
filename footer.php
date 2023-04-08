e7e20bf21fe480562a55c7b028f6ce8243df38b6
Pavan Kumar M <45788096+MrDevLovePer@users.noreply.github.com>
2023-04-08 16:33:51 +0530

    message

---
+++ footer.php
@@ -0,0 +1,32 @@
+
+
+<footer id="contactus" style="background-color: #272727;">
+    <div class="container" style="width: 100%;">
+				<div class="row justify-content-center">
+					<div class="col text-center">
+                        <img src="img/ftrlg.svg" style="width:60px;" alt="">
+                        <h2 class="footer-heading"> 
+                        <a href="index.php" style="color: #ffffff; font-family: 'Sora', sans-serif; font-size: 50px; text-decoration: none;"> Softufon Technologies </a> 
+                        </h2>
+                    </div>	
+                                                
+                    <div class="menu text-center">
+                    <p>
+                    <a href="index.php" style="color: #ffd6a5; padding-top: 50px; padding-right: 40px; font-family:Comfortaa;"> HOME </a> 
+                    <a href="#jbs" style="color: #ffd6a5; padding-top: 50px; padding-right: 40px; font-family:Comfortaa;"> JOBS </a> 
+                    <a href="mailto: Support@softufon.com" style="color: #ffd6a5; padding-top: 50px; font-family:Comfortaa;"> CONTACT </a>
+                    </p>
+                    </div>
+					
+				</div>
+				
+                <div class="row mt-5">
+					<div class="col-md-12 text-center">
+						<p class="copyrigt">
+                        <a style="color: #9FA4A9; text-decoration: none;"> All rights reserved 2023 | Developed by </a> 
+                        <a href="https://www.linkedin.com/in/DevLovePer" style="color: #FDFFAC;">DevLovePer, Softufon Technologies</a> 
+					    </p>
+					</div>
+				</div>
+    </div>
+</footer>
